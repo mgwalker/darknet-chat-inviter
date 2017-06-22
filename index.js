fb3ef0fc365598e275e52dcc7e0a4f49cddde3c8
Greg Walker <mgwalker@gmail.com>
2017-06-21 23:04:00 -0500

    initial

---
+++ index.js
@@ -0,0 +1,40 @@
+const request = require('request');
+
+module.exports.inviter = (e, context, callback) => {
+  const email = e.queryStringParameters.email;
+  const output = { message: 'Your invitation has been sent!' };
+  let statusCode = 500;
+
+  const options = { formData: {
+    token: process.env.SLACK_TOKEN,
+    email
+  }, json: true };
+
+  request.post('https://darkcooger.slack.com/api/users.admin.invite', options, (err, res) => {
+    if (err) {
+      output.message = err.message;
+    } else {
+      // If the account that owns the token is not admin, Slack will oddly
+      // return `200 OK`, and provide other information in the body. So we
+      // need to check for the correct account scope and call the callback
+      // with an error if it's not high enough.
+      const { ok, error: providedError, needed} = res.body
+      if (!ok) {
+        statusCode = 500;
+        if (providedError === 'missing_scope' && needed === 'admin') {
+          output.message = 'Missing admin scope: The token you provided is for an account that is not an admin. You must provide a token from an admin account in order to invite users through the Slack API.';
+        } else if (providedError === 'already_invited') {
+          output.message = 'You have already been invited to Slack. Check for an email from feedback@slack.com.';
+        } else if (providedError === 'already_in_team') {
+          output.message = 'You already belong to darknet chat, goober.';
+        } else {
+          output.message = providedError;
+        }
+      } else {
+        statusCode = 200;
+      }
+    }
+
+    callback(null, { statusCode: statusCode, body: JSON.stringify(output) });
+  });
+};
